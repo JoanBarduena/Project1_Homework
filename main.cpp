2384a5bcdfda0e2acd2e6328035b8c2afccfd729
JoanBarduena <36154195+JoanBarduena@users.noreply.github.com>
2018-03-07 22:34:15 +0100

    TextureTryFail

--- main.cpp
+++ main.cpp
@@ -14,30 +14,40 @@ int main(int argc, char* argv[]){
 	
 	SDL_Window *window;     
 	SDL_Renderer * renderer; 
+	SDL_Surface * surface; 
+	SDL_Texture * texture; 
 
 	SDL_Init(SDL_INIT_VIDEO);            
-
+	
 	window = SDL_CreateWindow("MyGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
 	//Renderer
-	renderer = SDL_CreateRenderer(window, -1, 0); 
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	//Square 
+	SDL_Rect rect;
 
-	SDL_Rect rect; 
-
-	rect.w = RECT_LENGTH; 
-	rect.h = RECT_LENGTH; 
-	rect.x = 525; 
+	rect.w = RECT_LENGTH;
+	rect.h = RECT_LENGTH;
+	rect.x = 525;
 	rect.y = 325;
 
 	//Bullet 
+	SDL_Rect bullet;
 
-	SDL_Rect bullet; 
-
-	bullet.w = 20; 
+	bullet.w = 20;
 	bullet.h = 10;
 
+	//PNG
+	SDL_Rect destination;
+	destination.x = 525;
+	destination.y = 325;
+	destination.w = RECT_LENGTH;
+	destination.h = RECT_LENGTH;
+
+	surface = IMG_Load("textures/RocketSDL.png");
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
 
 	bool isRunning = true; 
 	SDL_Event event; 
@@ -96,12 +106,10 @@ int main(int argc, char* argv[]){
 		SDL_SetRenderDrawColor(renderer, 16, 62, 166, 0);
 		SDL_RenderClear(renderer);
 
-		SDL_SetRenderDrawColor(renderer, 203, 32, 39, 0);
-		SDL_RenderFillRect(renderer, &rect);
+		//SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
+		//SDL_RenderFillRect(renderer, &bullet);
 
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
-		SDL_RenderFillRect(renderer, &bullet);
-
+		SDL_RenderCopy(renderer, texture, &rect, &destination); 
 		SDL_RenderPresent(renderer); 
 	}
 
